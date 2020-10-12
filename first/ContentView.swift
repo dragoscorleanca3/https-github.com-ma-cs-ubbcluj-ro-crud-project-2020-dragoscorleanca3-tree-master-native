5e363ee90f89daac670f7c40d95ed2bb5e85dc61
dragoscorleanca <dragoscorleanca@gmail.com>
2020-10-12 16:13:15 +0300

    Initial Commit

---
+++ first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first
+//
+//  Created by Dragos Corleanca on 10/12/20.
+//  Copyright © 2020 Dragos Corleanca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
